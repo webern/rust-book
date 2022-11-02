cce154188408da5c6774ae8f8901f03ed3b3ca09
Matt Briggs <matthew.james.briggs@gmail.com>
2022-11-01 20:47:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Notes from The Rust Book
+
+This repository is for a group that is studying *The Rust Programming Language* by Klabnik and Nichols.
---
+++ chapter_4/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/// [p 59](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html):
+/// - What is a garbage collector?
+/// - Does Rust have a garbage collector?
+/// - Is this good or bad?
+///
+/// [p 60](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#the-stack-and-the-heap)
+/// - What is the stack?
+/// - What is the heap?
+/// - What must we know about data in order to store it on the stack?
+/// - What is the advantage of having data on the stack instead of the heap?
+mod what_is_ownership {}
