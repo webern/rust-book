ce6ddc4ec24e83b9ee12af8c35c7e04e6e3db2dd
Matt Briggs <matthew.james.briggs@gmail.com>
2022-11-01 21:18:39 -0700

    chapter 4

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -12,4 +12,43 @@ fn main() {
 /// - What is the heap?
 /// - What must we know about data in order to store it on the stack?
 /// - What is the advantage of having data on the stack instead of the heap?
-mod what_is_ownership {}
+///
+/// [p 61](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#ownership-rules)
+/// - What problem does Rust's ownership concept try to solve?
+/// (Keeping track of what parts of code are using what data on the heap, minimizing the amout of
+/// duplicate data on the heap, cleaning up unused data on the heap.
+///
+mod what_is_ownership {
+    /// [p. 62](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#the-string-type):
+    /// - Where is the data stored for Rust's `String` type?
+    /// - Where is a string literal stored?
+    /// - When is the memory allocated by `String` automatically freed and given back to the
+    ///   operating system?
+    mod the_string_type {}
+
+    /// [p. 64](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#memory-and-allocation):
+    /// - What is the name of the special function that Rust calls automatically when a variable
+    ///   goes out of scope?
+    /// - What is this called in C++?
+    ///
+    /// [p. 65](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#ways-variables-and-data-interact-move)
+    /// What is happening on the stack and on the heap in this code?
+    /// ```
+    /// let s1 = String::from("hello");
+    /// let s2 = s1;
+    /// ```
+    ///
+    /// If Rust were stupid, what would happen when `s1` and `s2` go out of scope?
+    /// Instead what happens? (p. 66)
+    ///
+    /// What choice has Rust made about "deep" copies? (Bottom of p. 66)
+    /// Enter `Clone` and `fn clone() -> Self`
+    ///
+    /// [p. 67](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#ways-variables-and-data-interact-clone):
+    /// How is it that we don't need to call clone to get copies of integers and other primitives?
+    /// What is special about these primitives that make them so quick to copy?
+    /// Enter the `Copy` trait.
+    fn memory_and_allocation() {
+        // TODO - demo Clone and Copy if there's time
+    }
+}
