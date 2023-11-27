13339cf078324e64b6139464e7a0373ad3d27753
Matthew James Briggs <matthew.james.briggs@gmail.com>
2023-11-27 09:47:48 -0800

    chapter-8 template

---
+++ chapter-8/rustlings/exercises/04_primitive_types/primitive_types3.rs
@@ -0,0 +1,18 @@
+// primitive_types3.rs
+//
+// Create an array with at least 100 elements in it where the ??? is.
+//
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
+// for a hint.
+
+fn main() {
+    let a = "some string with more than 100 characters to satisfy this silly \
+    rustlings exercise.                                                             ";
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed")
+    }
+}
---
+++ chapter-8/src/main.rs
@@ -0,0 +1,15 @@
+/*!
+
+# Chapter 8: Collections
+
+pp. 131-149 in the 2018 edition paperback.
+
+ */
+#![allow(dead_code, unused_variables, unused_mut)]
+
+fn main() {
+    println!("Chapter 8!");
+}
+
+///
+fn all_collections() {}
